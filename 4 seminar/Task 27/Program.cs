875549d4537b2ad543c443c5c61f44e6f4ead69a
Mansur Dagaev <mans.4e4en@mail.ru>
2023-01-29 20:09:20 +0300

    second commit

---
+++ 4 seminar/Task 27/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+
+{
+Console.Write("Введи пятизначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(number));
+
+}
+
+public static int Sum(int n)
+
+{
+int sum = 0;
+while(n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+ return sum;      
+}
+}
+
+}
+
+
