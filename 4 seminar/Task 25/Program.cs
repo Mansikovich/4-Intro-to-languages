7244235a75af2185169b7815a9017d204853a17c
Mansur Dagaev <mans.4e4en@mail.ru>
2023-01-29 20:05:02 +0300

    first commit

---
+++ 4 seminar/Task 25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.Write("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Power(a,b));
+
+            
+            
+
+        }
+        public static double Power(double a, double b)
+            {
+                return  Math.Pow(a, b);
+                
+                
+            }
+
+    }
+}
