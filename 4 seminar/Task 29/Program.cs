8f420a9235772fa8598975b50b6d38a183978de6
Mansur Dagaev <mans.4e4en@mail.ru>
2023-01-30 11:05:04 +0300

    third update

---
+++ 4 seminar/Task 29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+
+{
+Console.Write("Введи числа через запятую ");
+string number = Console.ReadLine();
+Console.WriteLine(Array(number));
+
+}
+
+public static string Array(string number)
+
+{
+number = $"[{number}]";    
+ return number;      
+}
+}
+
+}
